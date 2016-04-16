b05e63744e4ae55d8feae4db0c347eacfd84bcc4
Joao Garin <joao.garin@gmail.com>
2016-04-16 10:44:45 +0200

    added prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,12 +5,14 @@
  * env(), getBanner(), root(), and rootDir()
  * are defined at the bottom
  */
-
 var webpack = require('webpack');
 var helpers = require('./helpers');
 
 var path = require('path');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+var CompressionPlugin = require('compression-webpack-plugin');
+var DedupePlugin = require('webpack/lib/optimize/DedupePlugin');
 
 var commonConfig = {
     resolve: {
@@ -90,7 +92,48 @@ var defaultConfig = {
     output: {
         publicPath: path.resolve(__dirname),
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        // Plugin: DedupePlugin
+        // Description: Prevents the inclusion of duplicate code into your bundle
+        // and instead applies a copy of the function at runtime.
+        //
+        // See: https://webpack.github.io/docs/list-of-plugins.html#defineplugin
+        // See: https://github.com/webpack/docs/wiki/optimization#deduplication
+        new DedupePlugin(),
+
+        new UglifyJsPlugin({
+            // to debug prod builds uncomment //debug lines and comment //prod lines
+
+            // beautify: true,//debug
+            // mangle: false,//debug
+            // dead_code: false,//debug
+            // unused: false,//debug
+            // deadCode: false,//debug
+            // compress : { screw_ie8 : true, keep_fnames: true, drop_debugger: false, dead_code: false, unused: false, }, // debug
+            // comments: true,//debug
+
+            beautify: false,//prod
+            // disable mangling because of a bug in angular2 beta.1, beta.2 and beta.3
+            // TODO(mastertinner): enable mangling as soon as angular2 beta.4 is out
+            // mangle: { screw_ie8 : true },//prod
+            mangle: false,
+            compress: { screw_ie8: true },//prod
+            comments: false//prod
+
+        }),
+        /**
+         * Plugin: CompressionPlugin
+         * Description: Prepares compressed versions of assets to serve
+         * them with Content-Encoding
+         *
+         * See: https://github.com/webpack/compression-webpack-plugin
+         */
+        new CompressionPlugin({
+            regExp: /\.css$|\.html$|\.js$|\.map$/,
+            threshold: 2 * 1024
+        })
+    ]
 }
 
 var webpackMerge = require('webpack-merge');
@@ -104,4 +147,3 @@ module.exports = [
     // Server
     webpackMerge({}, defaultConfig, commonConfig, serverConfig)
 ];
-
