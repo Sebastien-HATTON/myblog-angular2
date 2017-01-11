c2ef72d43fcc5899b1aca10636777c2deee9061d
Joao Garin <joao.garin@gmail.com>
2017-01-11 16:35:54 +0000

    use angular2-template-loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,23 +13,9 @@ import {HeaderComponent} from './components/Header/Header';
 import {NavSidebarComponent} from './components/NavSidebar/NavSidebar';
 import {AboutComponent} from './components/about/about';
 
-/*
- * Import the global css for the site
- * 
- * Because this component is set to have encapsulation : ViewEncapsulation.None
- * the css will be global just like normal css meaning it will not be tied to any component or 
- * scoped in any way.
- */
-var page_css = require('./css/layout/_page.scss');
-
-/**
- * Include the prism css file to be processed by webpack
- */
-var prism_css = require('./services/prism/prism.css');
-
 @Component({
     selector: 'mb-app',
-    styles: [`${page_css} ${prism_css}`],
+    styleUrls: ['./services/prism/prism.css','./css/layout/_page.scss'],
     encapsulation: ViewEncapsulation.None,
     template: `
     <mb-blog-header></mb-blog-header>
